775c6f7d5c44b3475c7c656739ad7f8fb1eb3583
Biplab <bbiswasccna@gmail.com>
2025-06-28 13:40:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<ProductList/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
